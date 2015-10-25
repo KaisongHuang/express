d0b7f29c4b89c739ce4aa7a63e42c14615c7753f
FrankHH <hh@frank1996.link>
2015-10-25 22:45:40 +0800

    hh_driver

---
+++ src/CentreTest/CentreBlService_Driver.java
@@ -0,0 +1,39 @@
+package CentreTest;
+
+import _enum.ResultMessage;
+import logic.centreblservice.CentreBlService;
+import vo.CentreArrivalVO;
+import vo.CentreTransforVO;
+import vo.DeliveryVO;
+import vo.PackVO;
+
+public class CentreBlService_Driver {
+	
+	public void drive(CentreBlService centre){
+		
+		ResultMessage result;
+		
+		result=centre.createDelivery(new DeliveryVO());
+		if(result==ResultMessage.Error){
+			System.out.println("ERROR!");
+		}
+		
+		result=centre.createPack(new PackVO());
+		if(result==ResultMessage.Error){
+			System.out.println("ERROR!");
+		}
+		
+		result=centre.createReceive(new CentreArrivalVO(0, null, 0, null, null));
+		if(result==ResultMessage.Error){
+			System.out.println("ERROR!");
+		}
+		
+		result=centre.manageTranfor(new CentreTransforVO(null, null, 0, 0, null, null, 0, null, null, 0));
+		if(result==ResultMessage.Error){
+			System.out.println("ERROR!");
+		}
+		
+		
+	}
+
+}
