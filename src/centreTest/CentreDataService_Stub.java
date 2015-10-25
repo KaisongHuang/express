7532ce3cab8c76082b6f703403a944591f1c8b24
KaisongHuang <huangkaisong@126.com>
2015-10-26 00:17:36 +0800

    add_warehouse_stub&driver_hks
    初次搭建warehouse的stub&driver.

--- src/centreTest/CentreDataService_Stub.java
+++ src/centreTest/CentreDataService_Stub.java
@@ -7,33 +7,31 @@ import data.centredataservice.CentreDataService;
 import po.CentrePO;
 
 public class CentreDataService_Stub implements CentreDataService {
-	
+
 	public CentrePO find(int id) throws RemoteException {
-		
+
 		System.out.println("Find!!");
-		
+
 		return new CentrePO();
-		
+
 	}
-	
+
 	public ResultMessage insert(CentrePO po) throws RemoteException {
-		
+
 		return ResultMessage.Error;
-		
+
 	}
-	
+
 	public ResultMessage delete(CentrePO po) throws RemoteException {
-		
+
 		return ResultMessage.Error;
-		
+
 	}
-	
+
 	public ResultMessage update(CentrePO po) throws RemoteException {
-		
+
 		return ResultMessage.Error;
-		
+
 	}
-	
-	
 
 }
--- src/warehouseTest/WarehouseBlService_Driver.java
+++ src/warehouseTest/WarehouseBlService_Driver.java
@@ -1,5 +1,35 @@
 package warehouseTest;
 
-public class WarehouseBlService_Driver {
+import vo.InStorageVO;
+import vo.OutStorageVO;
+import _enum.ResultMessage;
+import logic.warehouseblservice.WarehouseBlService;
 
+public class WarehouseBlService_Driver {
+	public void drive(WarehouseBlService warehouse) {
+		ResultMessage result;
+		result = warehouse.importGoods(new InStorageVO());
+		if (result == ResultMessage.Error)
+			System.out.println("ERROR!");
+
+		result = warehouse.exportGoods(new OutStorageVO());
+		if (result == ResultMessage.Error)
+			System.out.println("ERROR!");
+
+		result = warehouse.setAlarm();
+		if (result == ResultMessage.Error)
+			System.out.println("ERROR!");
+
+		result = warehouse.checkWarehouse(0, 0);
+		if (result == ResultMessage.Error)
+			System.out.println("ERROR!");
+
+		result = warehouse.summarizeWarehouse(0);
+		if (result == ResultMessage.Error)
+			System.out.println("ERROR!");
+
+		result = warehouse.initWarehouse(new InStorageVO());
+		if (result == ResultMessage.Error)
+			System.out.println("ERROR!");
+	}
 }
