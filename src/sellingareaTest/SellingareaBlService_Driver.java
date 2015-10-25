4bef5bc0a18256a2704f265e2519055e416d950d
FrankHH <hh@frank1996.link>
2015-10-25 22:53:33 +0800

    hh_修正

--- src/sellingareaTest/SellingareaBlService_Driver.java
+++ src/sellingareaTest/SellingareaBlService_Driver.java
@@ -14,21 +14,21 @@ public class SellingareaBlService_Driver {
       public void drive(SellingareaBlService sellingarea){
     	  ResultMessage result=sellingarea.manageCarPack(new CarPackVO() );
     	  if(result==ResultMessage.Error)
-    		  System.out.println("�������Ϣ����");
+    		  System.out.println("锟斤拷锟斤拷锟斤拷锟较拷锟斤拷锟�");
     	  result=sellingarea.createReceiving(new AcceptVO());
     	  if(result==ResultMessage.Fail)
-    		  System.out.println("��Ϣ�ϴ�ʧ��");
+    		  System.out.println("锟斤拷息锟较达拷失锟斤拷");
     	  result=sellingarea.createDelivery(new DeliverVO());
     	  if(result==ResultMessage.Success)
-    		  System.out.println("��Ϣ�ϴ��ɹ�");
+    		  System.out.println("锟斤拷息锟较达拷锟缴癸拷");
     	  result=sellingarea.createDebitnote(new ReceiptVO());
     	  if(result==ResultMessage.Success)
-    		  System.out.println("��Ϣ�ϴ��ɹ�");
+    		  System.out.println("锟斤拷息锟较达拷锟缴癸拷");
     	  result=sellingarea.manageCarinfo(new CarVO(), Operation.delete);
     	  if(result==ResultMessage.Fail)
-    		  System.out.println("��Ϣ�ϴ�ʧ��");
+    		  System.out.println("锟斤拷息锟较达拷失锟斤拷");
     	  result=sellingarea.manageDriverinfo(new DriverVO(), Operation.find);
     	  if(result==ResultMessage.Error)
-    		  System.out.println("�������Ϣ����");
+    		  System.out.println("锟斤拷锟斤拷锟斤拷锟较拷锟斤拷锟�");
       }
 }
