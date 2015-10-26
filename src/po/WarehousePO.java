058ef04c0993825a7d1dbb5c5d79e93a65d73d53
KaisongHuang <huangkaisong@126.com>
2015-10-26 13:10:58 +0800

    finish warehouse&finance

--- src/po/WarehousePO.java
+++ src/po/WarehousePO.java
@@ -1,5 +1,11 @@
 package po;
 
 public class WarehousePO {
+	int id;
+	String destination;
 
+	public WarehousePO(int id, String destination) {
+		this.id = id;
+		this.destination = destination;
+	}
 }
