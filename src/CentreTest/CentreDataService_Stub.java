24b00d715b9fe082e65ddffdb4064ada12ea69fb
KaisongHuang <huangkaisong@126.com>
2015-10-26 12:26:21 +0800

    centretest_commit_hks

---
+++ src/CentreTest/CentreDataService_Stub.java
@@ -0,0 +1,37 @@
+package CentreTest;
+
+import java.rmi.RemoteException;
+
+import _enum.ResultMessage;
+import data.centredataservice.CentreDataService;
+import po.CentrePO;
+
+public class CentreDataService_Stub implements CentreDataService {
+
+	public CentrePO find(int id) throws RemoteException {
+
+		System.out.println("Find!!");
+
+		return new CentrePO();
+
+	}
+
+	public ResultMessage insert(CentrePO po) throws RemoteException {
+
+		return ResultMessage.Error;
+
+	}
+
+	public ResultMessage delete(CentrePO po) throws RemoteException {
+
+		return ResultMessage.Error;
+
+	}
+
+	public ResultMessage update(CentrePO po) throws RemoteException {
+
+		return ResultMessage.Error;
+
+	}
+
+}
