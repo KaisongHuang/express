0ba31cc963f12c286ac43f446bf324f27713d5fd
KaisongHuang <huangkaisong@126.com>
2015-10-28 20:17:10 +0800

    sender_hks

--- src/senderTest/SenderBlService_Stub.java
+++ src/senderTest/SenderBlService_Stub.java
@@ -1,5 +1,6 @@
 package senderTest;
 
+import vo.SenderVO;
 import _enum.ResultMessage;
 import logic.senderblservice.SenderBlService;
 
@@ -8,6 +9,11 @@ public class SenderBlService_Stub implements SenderBlService {
 	public ResultMessage search(long id) {
 		// TODO Auto-generated method stub
 		return ResultMessage.Error;
+	}
+
+	public SenderVO search(int id) {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 }
