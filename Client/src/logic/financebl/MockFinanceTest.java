35b9d17c14492817185b2043f830a14ff27626fb
KaisongHuang <huangkaisong@126.com>
2015-11-16 10:54:04 +0800

    mock_hks

---
+++ Client/src/logic/financebl/MockFinanceTest.java
@@ -0,0 +1,39 @@
+package logic.financebl;
+
+import static org.junit.Assert.*;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import _enum.ResultMessage;
+import vo.AccountVO;
+import vo.FinanceVO;
+
+public class MockFinanceTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		double balance = new MockFinance().getBalance();
+
+		AccountVO find = new MockFinance().find(null);
+		FinanceVO getTotal = new MockFinance().getTotal(null, null);
+		FinanceVO checkReceipt = new MockFinance().checkReceipt(null, null);
+
+		ResultMessage createCost = new MockFinance().createCost(null);
+		ResultMessage initAccount = new MockFinance().initAccount(null, 0);
+		ResultMessage manageAccount = new MockFinance().manageAccount(null, null);
+
+		assertEquals(true, balance == 0.0);
+		assertEquals(find, null);
+		assertEquals(getTotal, null);
+		assertEquals(checkReceipt, null);
+		assertEquals(createCost, ResultMessage.Success);
+		assertEquals(initAccount, ResultMessage.Success);
+		assertEquals(manageAccount, ResultMessage.Success);
+	}
+
+}
