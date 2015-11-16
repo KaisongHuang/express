fa3d5d9cc11479609c80f35ff4387d64d1a0f1f0
Njuhuwen <1298584902@qq.com>
2015-11-16 10:55:16 +0800

    hw

--- Client/src/senderTest/SenderBlService_Driver.java
+++ Client/src/senderTest/SenderBlService_Driver.java
@@ -1,14 +1,14 @@
 package senderTest;
 
 import logic.senderblservice.SenderBlService;
-
+import vo.HistoryVO;
 import vo.SenderVO;
 
 public class SenderBlService_Driver {
 	
 	public void drive(SenderBlService sender){
 		
-		SenderVO result = sender.search(0);
+		HistoryVO result = sender.search(0);
 		if(result==null){
 			System.out.println("Not Found!");
 		}
