93ab0b9f98c33647a529d35a1c1bcefe39df61e6
Njuhuwen <1298584902@qq.com>
2015-11-17 19:16:31 +0800

    hw_DataBase

--- Server/src/MySQLDataBase.java
+++ Server/src/MySQLDataBase.java
@@ -1,10 +1,14 @@
+import java.beans.Statement;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+
+import Server_enum.ResultMessage;
 
 
 public class MySQLDataBase {
-      public static Connection getMySQL_Con(String driverClassName,String serverHost,String serverPort,String dbName,String userName,
+      public  Connection getMySQL_Con(String driverClassName,String serverHost,String serverPort,String dbName,String userName,
     		  String password){
     	  if(driverClassName==null)
     		  driverClassName="com.mysql.jdbc.Driver";
@@ -19,7 +23,7 @@ public class MySQLDataBase {
     	  return getConnection(driverClassName,dbURL,userName,password);
       }
       
-      public static Connection getConnection(String driverClassName,String dbURL,String userName,String password){
+      public  Connection getConnection(String driverClassName,String dbURL,String userName,String password){
     	  Connection con=null;
     	  try {
 			Class.forName(driverClassName);
@@ -34,5 +38,75 @@ public class MySQLDataBase {
 			e.printStackTrace();
 		}
     	  return con;
+      }
+      
+      public  ResultMessage insert(Connection con,String tableName,Object ob){
+    	  String sql=getInsertSql(tableName,ob);
+    	  Statement st;
+    	  try{
+    		  st=(Statement) con.createStatement();
+    		  int count= ((java.sql.Statement) st).executeUpdate(sql);
+    		  System.out.println("向"+tableName+"中插入"+count+"条数据成功");
+    	  }catch(SQLException ex){
+    		  return ResultMessage.Fail;
+    	  }
+    	  return ResultMessage.Success;
+      }
+      
+      
+      public static Object find(Connection con,String tableName,Object ob){
+    	  String sql=getSelectSql(tableName,ob);
+    	  Statement st;
+    	  try{
+    		  st=(Statement) con.createStatement();
+    		  ResultSet rs=((java.sql.Statement) st).executeQuery(sql);
+    		  
+    	  }catch(SQLException ex){
+    		  return null;
+    	  }
+    	  return null;
+      }
+      
+      public  ResultMessage delete(Connection con,String tableName,Object ob){
+    	  String sql=getDeleteSql(tableName,ob);
+    	  Statement st;
+    	  try{
+    		  st=(Statement) con.createStatement();
+    		  ((java.sql.Statement) st).executeQuery(sql);
+    		  System.out.println("成功删除");
+    	  }catch(SQLException ex){
+    		  return ResultMessage.Fail;
+    	  }
+    	  return ResultMessage.Success;
+    	  
+      }
+      
+      public  ResultMessage update(Connection con,String tableName,Object ob){
+    	  String sql=getUpdateSql(tableName,ob);
+    	  Statement st;
+    	  try{
+    		  st=(Statement) con.createStatement();
+    		  ((java.sql.Statement) st).executeQuery(sql);
+    	  }catch(SQLException ex){
+    		  return ResultMessage.Fail;
+    	  }
+    	  return ResultMessage.Success;
+    	  
+      }
+      public static String getSelectSql(String tableName,Object ob){
+    	  return null;
+      }
+      
+      public static String getUpdateSql(String tableNmae,Object ob){
+    	  return null;
+      }
+      
+      public static String getDeleteSql(String tableName,Object ob){
+    	  return null;
+      }
+      public static String getInsertSql(String tableName,Object ob){
+    	  
+    	  return null;
+    	  
       }
 }
