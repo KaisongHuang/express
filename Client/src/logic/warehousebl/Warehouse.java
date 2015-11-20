f92c6c4ad517626131b0d3f3bbe474b9568b866f
KaisongHuang <huangkaisong@126.com>
2015-11-20 20:46:13 +0800

    hks

--- Client/src/logic/warehousebl/Warehouse.java
+++ Client/src/logic/warehousebl/Warehouse.java
@@ -1,5 +1,40 @@
 package logic.warehousebl;
 
-public class Warehouse {
+import _enum.ResultMessage;
+import logic.warehouseblservice.WarehouseBlService;
+import vo.InStorageVO;
+import vo.OutStorageVO;
+
+public class Warehouse implements WarehouseBlService{
+
+	public ResultMessage importGoods(InStorageVO vo) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public ResultMessage exportGoods(OutStorageVO vo) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public ResultMessage setAlarm() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public ResultMessage checkWarehouse(int begin, int end) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public ResultMessage summarizeWarehouse(int end) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public ResultMessage initWarehouse(InStorageVO vo) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
