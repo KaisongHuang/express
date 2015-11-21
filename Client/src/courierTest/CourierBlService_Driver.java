920f5bdd0bdc98c2db5f885e62c42001a65c3670
FrankHH <hh@frank1996.link>
2015-11-21 12:31:09 +0800

    hh_更新4

--- Client/src/courierTest/CourierBlService_Driver.java
+++ Client/src/courierTest/CourierBlService_Driver.java
@@ -10,7 +10,7 @@ public class CourierBlService_Driver {
 		System.out.println(courierBlService.getPrice(10, 20));
 		System.out.println(courierBlService.getTime(1000));
 		ResultMessage result;
-		result = courierBlService.OrderInput(new SenderVO());
+		result = courierBlService.OrderInput(new SenderVO(), null);
 		if (result == ResultMessage.Success)
 			System.out.println("Success!");
 		result = courierBlService.ConsigneeinfoInput(new CourierVO(0, null, null));
