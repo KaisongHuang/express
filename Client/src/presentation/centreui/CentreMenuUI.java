10195bc1ded511bf4f7cf1b4e23aa29aa5285368
FrankHH <hh@frank1996.link>
2015-11-21 15:48:01 +0800

    hh

--- Client/src/presentation/centreui/CentreMenuUI.java
+++ Client/src/presentation/centreui/CentreMenuUI.java
@@ -14,7 +14,8 @@ import javax.swing.JTextField;
 
 public class CentreMenuUI extends JPanel{
 
-
+	JButton btnNewButton_7;
+	JButton btnNewButton_8;
 
 	/**
 	 * Create the application.
@@ -34,14 +35,16 @@ public class CentreMenuUI extends JPanel{
 		
 		this.setLayout(null);
 		
-		JButton btnNewButton_8 = new JButton("\u88C5\u8FD0\u7BA1\u7406");
+		btnNewButton_8 = new JButton("\u88C5\u8FD0\u7BA1\u7406");
 		btnNewButton_8.setBounds(137, 0, 137, 55);
 		this.add(btnNewButton_8);
 		
-		JButton btnNewButton_7 = new JButton("\u63A5\u53D7\u5355");
+		btnNewButton_7 = new JButton("\u63A5\u53D7\u5355");
 		btnNewButton_7.setBackground(new Color(255, 248, 220));
 		btnNewButton_7.setBounds(0, 0, 137, 55);
 		this.add(btnNewButton_7);
 		
 	}
+	
+	
 }
