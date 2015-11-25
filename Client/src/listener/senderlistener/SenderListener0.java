42cec637922be6734d90c8e377157dd1f6ab8d74
Njuhuwen <1298584902@qq.com>
2015-11-25 19:19:02 +0800

    hw

--- Client/src/listener/senderlistener/SenderListener0.java
+++ Client/src/listener/senderlistener/SenderListener0.java
@@ -1,5 +1,23 @@
 package listener.senderlistener;
 
-public class SenderListener0 {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import logic.senderbl.Sender;
+import presentation.senderui.SearchUI;
+import vo.HistoryVO;
+
+public class SenderListener0 implements ActionListener{
+    SearchUI search;
+    Sender sender;
+	public SenderListener0(SearchUI s){
+		search=s;
+		sender=new Sender();
+	}
+	public void actionPerformed(ActionEvent e) {
+		String s=search.gettextFieldContent();
+		HistoryVO h= sender.search(Integer.parseInt(s));
+		
+	}
 
 }
--- Client/src/listener/senderlistener/SenderListener1.java
+++ Client/src/listener/senderlistener/SenderListener1.java
@@ -1,5 +1,18 @@
 package listener.senderlistener;
 
-public class SenderListener1 {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import presentation.senderui.SearchUI1;
+
+public class SenderListener1 implements ActionListener{
+	SearchUI1 search;
+    public SenderListener1(SearchUI1 s){
+    	search=s;
+    }
+	public void actionPerformed(ActionEvent e) {
+	     
+		
+	}
 
 }
