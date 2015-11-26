650052642bcc16d1d43bc0c2605f81523b675fdb
Njuhuwen <1298584902@qq.com>
2015-11-26 18:24:56 +0800

    hw

--- Client/src/presentation/mainui/MainFrame.java
+++ Client/src/presentation/mainui/MainFrame.java
@@ -1,9 +1,19 @@
 package presentation.mainui;
 
-public class MainFrame {
+import javax.swing.*;
 
+
+public class MainFrame {
+    JFrame frame;
+    JPanel panel;
+    
+	public static void main(String[] args){
+		new MainFrame().init();
+	}
 	public void init() {
-		// TODO Auto-generated method stub
+		frame=new JFrame();
+		frame.setBounds(100,100,900,600);
+		frame.setVisible(true);
 		
 	}
 
