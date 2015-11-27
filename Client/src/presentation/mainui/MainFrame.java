76f158f812e23dd04df34e811893a01cdf6d2f79
Njuhuwen <1298584902@qq.com>
2015-11-27 19:28:06 +0800

    hw

--- Client/src/presentation/mainui/MainFrame.java
+++ Client/src/presentation/mainui/MainFrame.java
@@ -1,20 +1,8 @@
 package presentation.mainui;
 
-import javax.swing.*;
-
-
 public class MainFrame {
-    JFrame frame;
-    JPanel panel;
     
-	public static void main(String[] args){
-		new MainFrame().init();
-	}
 	public void init() {
-		frame=new JFrame();
-		frame.setBounds(100,100,900,600);
-		frame.setVisible(true);
-		
 	}
 
 }
--- Client/src/presentation/mainui/Test.java
+++ Client/src/presentation/mainui/Test.java
@@ -1,10 +1,4 @@
 package presentation.mainui;
 
-import javax.swing.JFrame;
-
 public class Test {
-      public static void main(String[] args){
-    	  JFrame frame=new JFrame();
-    	  frame.setVisible(true);
-      }
 }
