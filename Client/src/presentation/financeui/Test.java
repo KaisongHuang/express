5dc9579e6148162f8ff4e1ac8fe1413b6c4f40d4
KaisongHuang <huangkaisong@126.com>
2015-12-08 21:17:57 +0800

    hks

--- Client/src/presentation/financeui/Test.java
+++ Client/src/presentation/financeui/Test.java
@@ -4,12 +4,12 @@ import javax.swing.JFrame;
 
 
 public class Test {
-	public static void main(String args[]){
-		JFrame jf =  new JFrame();
-		jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		FinanceUI4 ui =  new FinanceUI4();
-		jf.add(ui);
-		jf.setBounds(150, 150, 800, 500);
-		jf.setVisible(true);
-	}
+//	public static void main(String args[]){
+//		JFrame jf =  new JFrame();
+//		jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//		FinanceUI4 ui =  new FinanceUI4();
+//		jf.add(ui);
+//		jf.setBounds(150, 150, 800, 500);
+//		jf.setVisible(true);
+//	}
 }
