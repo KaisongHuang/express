1bb658a49f5e125f19c01b5ec7f49da0aa25fbe4
FrankHH <hh@frank1996.link>
2015-12-16 19:21:17 +0800

    hh_ui

--- Client/src/presentation/managerui/ManagerMenuUI.java
+++ Client/src/presentation/managerui/ManagerMenuUI.java
@@ -24,6 +24,7 @@ public class ManagerMenuUI extends JPanel{
 	private MyButton button5;
 	private int width;
 	private int height;
+	private int label_height;
 	ManagerMenuListener managermenulistener;
 
 	/**
@@ -37,6 +38,7 @@ public class ManagerMenuUI extends JPanel{
 		this.card = card;
 		this.width=width;
 		this.height=height;
+		this.label_height=height/10;
 		initialize();
 	}
 
@@ -47,35 +49,35 @@ public class ManagerMenuUI extends JPanel{
 		this.setSize(width,height);
 		this.setLayout(null);
 
-		button1 = new MyButton();//"机构管理"
-		button1.setBounds(0, 0, width, height/5);
+		button1 = new MyButton("机构管理","导航/manager/机构管理.png");//"机构管理"
+		button1.setBounds(0, label_height * 2, width, label_height);
 		button1.setClicked(true);
 		button1.addActionListener(managermenulistener);
 		button1.addMouseListener(managermenulistener);
 		this.add(button1);
 
-		button2 = new MyButton();//"人员管理"
+		button2 = new MyButton("人员管理","/导航/manager/人员管理.png");//"人员管理"
 
-		button2.setBounds(0, height/5, width, height/5);
+		button2.setBounds(0, label_height * 3, width, label_height);
 		button2.addActionListener(managermenulistener);
 		button2.addMouseListener(managermenulistener);
 		this.add(button2);
 
-		button3 = new MyButton();//"审批单据"
-		button3.setBounds(0, 2*height/5, width, height/5);
+		button3 = new MyButton("审批单据","/导航/manager/审批单据.png");//"审批单据"
+		button3.setBounds(0, label_height * 4, width, label_height);
 		button3.addActionListener(managermenulistener);
 		button3.addMouseListener(managermenulistener);
 		this.add(button3);
 
-		button4 = new MyButton();//"查看统计分析"
-		button4.setBounds(0, 3*height/5, width, height/5);
+		button4 = new MyButton("查看报表","/导航/manager/查看报表.png");//"查看统计分析"
+		button4.setBounds(0, label_height * 5, width, label_height);
 		button4.addActionListener(managermenulistener);
 		button4.addMouseListener(managermenulistener);
 		this.add(button4);
 
 
-		button5 = new MyButton();//"制定"
-		button5.setBounds(0, 4*height/5, width, height/5);
+		button5 = new MyButton("制定策略","/导航/manager/制定策略.png");//"制定"
+		button5.setBounds(0, label_height * 6, width, label_height);
 		button5.addActionListener(managermenulistener);
 		button5.addMouseListener(managermenulistener);
 		this.add(button5);
