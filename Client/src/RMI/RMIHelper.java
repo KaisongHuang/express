271c34dd2cad76d28eace0c931ca4cb135102dcf
Njuhuwen <1298584902@qq.com>
2015-12-23 11:22:10 +0800

    hw

--- Client/src/RMI/RMIHelper.java
+++ Client/src/RMI/RMIHelper.java
@@ -16,7 +16,7 @@ import dataservice.senderdataservice.SenderDataBaseService;
 import dataservice.warehousedataservice.WareHouseDataBaseService;
 
 public class RMIHelper {
-    private static String ip="172.16.95.6";
+    private static String ip="192.168.1.11";
     private static int port=3333;
 	private static AdminDataBaseService ad;
 	private static CentreDataBaseService cd;
