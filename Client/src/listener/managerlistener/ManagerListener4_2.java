55e9a21440a07f4b1daeaf3658923bc16a70c620
KaisongHuang <huangkaisong@126.com>
2015-12-30 21:34:26 +0800

    managerUI4_2完成

--- Client/src/listener/managerlistener/ManagerListener4_2.java
+++ Client/src/listener/managerlistener/ManagerListener4_2.java
@@ -6,12 +6,9 @@ import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.util.Vector;
 
-import _enum.Opera;
-import _enum.Operation;
 import logic.managerbl.Manager;
 import logic.managerblservice.ManagerBlService;
 import presentation.managerui.ManagerUI4_2;
-import vo.AccountVO;
 import vo.DistanceAndFeeVO;
 
 public class ManagerListener4_2 implements MouseListener, ActionListener {
@@ -30,21 +27,22 @@ public class ManagerListener4_2 implements MouseListener, ActionListener {
 			String city1=ui.getMyTextField().getText();
 			String city2=ui.getMyTextField_1().getText();
 			double distance=Double.parseDouble(ui.getMyTextField_2().getText());
-			double fee=Double.parseDouble(ui.getTextField_1().getText());
+			double fee=Double.parseDouble(ui.getMyTextField_3().getText());
 			
 			Vector<Object> rowData=new Vector<Object>();
 			rowData.add(city1);
 			rowData.add(city2);
 			rowData.add(distance);
+			rowData.add(fee);
 			
-			manager.manageDistance(new DistanceAndFeeVO(city1,city2,distance,fee), Opera.);
+			manager.insertCity(new DistanceAndFeeVO(city1,city2,distance,fee));
 			ui.getModel().addRow(rowData);
 		}
 		if (e.getSource() == ui.getMyButton_1()) {
 			String city1=ui.getMyTextField().getText();
 			String city2=ui.getMyTextField_1().getText();
 			double distance=Double.parseDouble(ui.getMyTextField_2().getText());
-			double fee=Double.parseDouble(ui.getTextField_1().getText());
+			double fee=Double.parseDouble(ui.getMyTextField_3().getText());
 			
 			int selectedRow = ui.getTable().getSelectedRow();// 获得选中行的索引
 			if (selectedRow != -1) // 是否存在选中行
@@ -53,18 +51,19 @@ public class ManagerListener4_2 implements MouseListener, ActionListener {
 				ui.getModel().setValueAt(city1, selectedRow, 0);
 				ui.getModel().setValueAt(city2, selectedRow, 1);
 				ui.getModel().setValueAt(distance, selectedRow, 2);
+				ui.getModel().setValueAt(fee, selectedRow, 3);
 				// table.setValueAt(arg0, arg1, arg2)
 			}
-			manager.manageDistance(new DistanceAndFeeVO(city1,city2,distance,fee), Opera.);
+			manager.updateCity(new DistanceAndFeeVO(city1,city2,distance,fee));
 		}
 		if (e.getSource() == ui.getMyButton_2()) {
 			int selectedRow = ui.getTable().getSelectedRow();// 获得选中行的索引
 			String city1=ui.getMyTextField().getText();
 			String city2=ui.getMyTextField_1().getText();
 			double distance=Double.parseDouble(ui.getMyTextField_2().getText());
-			double fee=Double.parseDouble(ui.getTextField_1().getText());
+			double fee=Double.parseDouble(ui.getMyTextField_3().getText());
 			
-			manager.manageDistance(new DistanceAndFeeVO(city1,city2,distance,fee), Opera.);
+			manager.deleteCity(new DistanceAndFeeVO(city1,city2,distance,fee));
 
 			if (selectedRow != -1) // 存在选中行
 				ui.getModel().removeRow(selectedRow); // 删除行
@@ -72,10 +71,7 @@ public class ManagerListener4_2 implements MouseListener, ActionListener {
 			ui.getMyTextField().setText("");
 			ui.getMyTextField_1().setText("");
 			ui.getMyTextField_2().setText("");
-		}
-		if (e.getSource() == ui.getMyButton_3()) {
-			double fee=Double.parseDouble(ui.getTextField_1().getText());
-			manager.updateFee(fee);
+			ui.getMyTextField_3().setText("");
 		}
 	}
 
@@ -87,10 +83,12 @@ public class ManagerListener4_2 implements MouseListener, ActionListener {
 			String city1=(String) ui.getModel().getValueAt(selectedRow, 0);
 			String city2=(String) ui.getModel().getValueAt(selectedRow, 1);
 			double distance=(Double) ui.getModel().getValueAt(selectedRow, 1);
+			double fee=(Double) ui.getModel().getValueAt(selectedRow, 3);
 			
 			ui.getMyTextField().setText(city1);
 			ui.getMyTextField_1().setText(city2); // 给文本框赋值
 			ui.getMyTextField_2().setText(""+distance);
+			ui.getMyTextField_3().setText(""+fee);
 		}
 	}
 
--- Client/src/presentation/managerui/ManagerUI4_2.java
+++ Client/src/presentation/managerui/ManagerUI4_2.java
@@ -5,6 +5,7 @@
 package presentation.managerui;
 
 import java.awt.Color;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Vector;
 
@@ -16,10 +17,13 @@ import javax.swing.JLabel;
 import javax.swing.JButton;
 
 import listener.managerlistener.ManagerListener4_2;
+import logic.managerbl.Manager;
+import logic.managerblservice.ManagerBlService;
 import presentation.MySwing.MyButton;
 import presentation.MySwing.MyDialog;
 import presentation.MySwing.MyTable;
 import presentation.MySwing.MyTextField;
+import vo.DistanceAndFeeVO;
 
 public class ManagerUI4_2 extends JPanel {
 
@@ -27,24 +31,23 @@ public class ManagerUI4_2 extends JPanel {
 	 *
 	 */
 	private static final long serialVersionUID = 1L;
-	private MyTextField textField_1;
 	private MyTextField myTextField;
 	private MyTextField myTextField_1;
 	private MyTextField myTextField_2;
-
+	private MyTextField myTextField_3;
 	private MyButton myButton;
 	private MyButton myButton_1;
 	private MyButton myButton_2;
-	private MyButton myButton_3;
 
 	private MyTable table;
 	private JScrollPane JSP;
 	private Vector<String> name;
 	private Vector<Object> data;
 	private DefaultTableModel model;
-
+	private ManagerBlService manager = new Manager();
 	private MyDialog dialog;
 	private ManagerListener4_2 managerlistener;
+	private JLabel label;
 
 	/**
 	 * Create the application.
@@ -68,7 +71,19 @@ public class ManagerUI4_2 extends JPanel {
 		dialog = new MyDialog();
 		this.add(dialog);
 
-		String names[] = { "城市A", "城市B", "距离(KM)" };
+		ArrayList<DistanceAndFeeVO> arr=new ArrayList<DistanceAndFeeVO>();
+		arr=manager.findCity();
+		
+		for(int i=0;i<arr.size();i++){
+			Vector<Object> rowData=new Vector<Object>();
+			rowData.add(arr.get(i).getCity1());
+			rowData.add(arr.get(i).getCity2());
+			rowData.add(arr.get(i).getDistance());
+			rowData.add(arr.get(i).getFee());
+			data.add(rowData);
+		}
+		
+		String names[] = { "城市A", "城市B", "距离(KM)","价格(每千公里)" };
 		name = new Vector<String>(Arrays.asList(names));
 		table = new MyTable(data, name);
 		table.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
@@ -80,69 +95,65 @@ public class ManagerUI4_2 extends JPanel {
 		table.setFocusable(false);
 		model = (DefaultTableModel) table.getModel();
 		JSP = new JScrollPane(table);
-		JSP.setBounds(21, 90, 340, 272);
+		JSP.setBounds(21, 90, 444, 272);
 		this.add(JSP);
 
-		JLabel label = new JLabel("价格(每千公里):");
-		label.setBounds(21, 60, 150, 18);
-		add(label);
-
 		JLabel lbla = new JLabel("城市A：");
-		lbla.setBounds(392, 101, 61, 16);
+		lbla.setBounds(477, 101, 61, 16);
 		add(lbla);
 
 		JLabel lblb = new JLabel("城市B：");
-		lblb.setBounds(392, 161, 61, 16);
+		lblb.setBounds(477, 135, 61, 16);
 		add(lblb);
 
 		JLabel lblkm = new JLabel("距离(KM)：");
-		lblkm.setBounds(392, 220, 73, 16);
+		lblkm.setBounds(477, 169, 73, 16);
 		add(lblkm);
-
-		textField_1 = new MyTextField();
-		textField_1.setBounds(115, 54, 122, 30);
-		add(textField_1);
-		textField_1.setColumns(10);
 
 		myTextField = new MyTextField();
 		myTextField.setColumns(10);
-		myTextField.setBounds(465, 94, 122, 30);
+		myTextField.setBounds(550, 94, 122, 30);
 		add(myTextField);
 
 		myTextField_1 = new MyTextField();
 		myTextField_1.setColumns(10);
-		myTextField_1.setBounds(465, 155, 122, 30);
+		myTextField_1.setBounds(550, 128, 122, 30);
 		add(myTextField_1);
 
 		myTextField_2 = new MyTextField();
 		myTextField_2.setColumns(10);
-		myTextField_2.setBounds(465, 214, 122, 30);
+		myTextField_2.setBounds(550, 162, 122, 30);
 		add(myTextField_2);
 
 		myButton = new MyButton("添加");
-		myButton.setBounds(392, 260, 195, 30);
+		myButton.setBounds(477, 260, 195, 30);
 		myButton.addActionListener(managerlistener);
 		add(myButton);
 
 		myButton_1 = new MyButton("修改");
-		myButton_1.setBounds(392, 296, 195, 30);
+		myButton_1.setBounds(477, 297, 195, 30);
 		myButton_1.addActionListener(managerlistener);
 		add(myButton_1);
 
 		myButton_2 = new MyButton("删除");
-		myButton_2.setBounds(392, 332, 195, 30);
+		myButton_2.setBounds(477, 332, 195, 30);
 		myButton_2.addActionListener(managerlistener);
 		add(myButton_2);
+		
+		label = new JLabel("价格：");
+		label.setBounds(477, 204, 86, 16);
+		add(label);
+		
+		myTextField_3 = new MyTextField();
+		myTextField_3.setColumns(10);
+		myTextField_3.setBounds(550, 197, 122, 30);
+		add(myTextField_3);
 
-		myButton_3 = new MyButton("设定");
-		myButton_3.setBounds(243, 54, 118, 30);
-		add(myButton_3);
-
-		setData();
 	}
 
-	public MyTextField getTextField_1() {
-		return textField_1;
+
+	public MyTextField getMyTextField_3() {
+		return myTextField_3;
 	}
 
 	public MyTextField getMyTextField() {
@@ -176,19 +187,8 @@ public class ManagerUI4_2 extends JPanel {
 	public MyButton getMyButton_2() {
 		return myButton_2;
 	}
-
-	public MyButton getMyButton_3() {
-		return myButton_3;
-	}
-
+	
 	public void setText(String s) {
 		dialog.setText(s);
-	}
-
-	private void setData() {
-		this.myTextField.setText();
-		this.myTextField_1.setText();
-		this.myTextField_2.setText();
-		this.textField_1.setText();
 	}
 }
